7e4c641c398ad671053661f70e58d1dd2be4a1d2
hardy-awan <57611165+hardy-awan@users.noreply.github.com>
2020-01-11 21:20:48 +0700

    Add files via upload

---
+++ src/component/CartList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CartItem from './CartItem';
+
+export default function CartColumns({value}) {
+    const {cart} =value;
+    return(
+        <div>
+           {cart.map(item => {
+          return <CartItem key={item.id} item={item} value={value} />;
+        })}
+        </div>
+    );
+
+};
